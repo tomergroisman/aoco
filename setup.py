119072bb2fe6dec5216dac3c91d3bb4bdcaeb29c
Tomer Groisman <tomergroisman@gmail.com>
2024-10-28 17:44:34 +0200

    feat: setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='aoco',
+    version='1.0',
+    description='Advent of Code runner cli application',
+    author='Tomer Groisman',
+    packages=['aoco'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'aoco = aoco.__main__:main',
+        ],
+    },
+)
