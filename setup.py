33baf7795406c816cf895a548d0f9c811e3bcead
Tomer Groisman <tomergroisman@gmail.com>
2024-10-28 18:07:35 +0200

    wip

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='1.0',
     description='Advent of Code runner cli application',
     author='Tomer Groisman',
-    packages=['aoco'],
+    packages=find_packages('aoco'),
     install_requires=requirements,
     entry_points={
         'console_scripts': [
