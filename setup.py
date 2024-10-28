f34455f0cb25be68e2ddb60d6ef38da0cd91daa5
Tomer Groisman <tomergroisman@gmail.com>
2024-10-28 19:43:40 +0200

    wip

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+
 
 setup(
     name='aoco',
     version='1.0',
     description='Advent of Code runner cli application',
     author='Tomer Groisman',
-    packages=find_packages(where="./aoco"),
+    packages=["aoco"],
     install_requires=requirements,
     entry_points={
         'console_scripts': [
