68d8d6a6d2fa47d02d9277e8642137cb27cb92df
Tomer Groisman <tomergroisman@gmail.com>
2024-12-16 21:56:04 +0200

    chore: version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='aoco',
-    version='1.1.0',
+    version='1.2.0',
     description='Advent of Code runner cli application',
     author='Tomer Groisman',
     packages=["aoco"],
